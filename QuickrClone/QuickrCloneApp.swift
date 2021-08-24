04f99a24da3d4035af342ba0587f683d902915ad
aarish21 <aarish.jerry@gmail.com>
2021-08-24 21:41:26 +0530

    Initial Commit

---
+++ QuickrClone/QuickrCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuickrCloneApp.swift
+//  QuickrClone
+//
+//  Created by Aarish Rahman on 24/08/21.
+//
+
+import SwiftUI
+
+@main
+struct QuickrCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
