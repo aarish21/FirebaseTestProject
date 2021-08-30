808cb5cdf1b70e8a90010cd8a6cd8f9c2f902249
aarish21 <aarish.jerry@gmail.com>
2021-08-30 20:57:29 +0530

    fixed errors

--- QuickrClone/View/ListingView.swift
+++ QuickrClone/View/ListingView.swift
@@ -61,3 +61,17 @@ struct ListingView_Previews: PreviewProvider {
         ListingView()
     }
 }
+extension String{
+    func load() -> UIImage{
+        do {
+            guard let url = URL(string: self) else {
+                return UIImage()
+            }
+            let data: Data = try Data(contentsOf: url)
+            return UIImage(data: data) ?? UIImage()
+        } catch  {
+            
+        }
+        return UIImage()
+    }
+}
